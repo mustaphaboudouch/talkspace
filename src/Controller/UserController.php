c6d6fc7d847b6db6709a8775bbcf7cd8308f38ae
vietanh2810 <vietanhc12@gmail.com>
2023-02-02 10:20:22 +0100

    Feat: add admin page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,10 +3,16 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 #[IsGranted('ROLE_ADMIN')]
 #[Route('/user', name: 'app_user_')]
@@ -20,5 +26,34 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
             'users' => $userRepository->search($request, $request->query->getInt('limit',10)),
         ]);
+    }
+
+    #[Route('/{id}/update', name: 'update', requirements: ['id' => '\d+'], methods: ['GET','POST'])]
+    public function update(User $user, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index');
+        }
+
+        return $this->render('user/update.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/{id}/remove/{token}', name: 'remove', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function remove(User $user, string $token, UserRepository $userRepository): Response
+    {
+        if (!$this->isCsrfTokenValid('remove' . $user->getId(), $token)) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $userRepository->remove($user, true);
+
+        return $this->redirectToRoute('app_user_index');
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\CallbackTransformer;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', null, [
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => "ROLE_USER",
+                    'Medecin' => "ROLE_DOCTOR",
+                    'Administrateur' => "ROLE_ADMIN",
+                ],
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ]);
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
