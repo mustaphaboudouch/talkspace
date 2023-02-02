2a717232ad0de9eb9fb05c9aa9b9225632de366b
Cheikh Tratra <chiekhou.traore@gmail.com>
2023-02-02 16:47:23 +0100

    feature add-page-forgot-password

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -14,14 +14,18 @@ class ResetPasswordRequestFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-                'attr' => ['autocomplete' => 'email'],
+                'attr' => [
+                    'autocomplete' => 'email',
+                    'class' => "form-control"
+                ],
+                'label' => 'Email',
+
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter your email',
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
