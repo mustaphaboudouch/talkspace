12a103869319cf678db7e5ce45bfaf1a072dfb82
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-02-05 14:24:14 +0100

    ci: setup prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./assets/**/*.js', './templates/**/*.html.twig'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
