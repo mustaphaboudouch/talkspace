b95eb0ae5b1e6a7ce9c88236ebadb70ed4d18bf2
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-02-05 14:47:03 +0100

    refactor: clean project

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('dashboard/index.html.twig');
     }
 }
