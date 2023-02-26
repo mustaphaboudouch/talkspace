d97a0648bf39be545b610e2c9ce05b9dcee970a3
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-02-26 13:30:21 +0100

    fix: fix migrations for heroku

--- migrations/Version20230226122738.php
+++ migrations/Version20230226122738.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230221213034 extends AbstractMigration
+final class Version20230226122738 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -25,13 +25,12 @@ final class Version20230221213034 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE contact_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE day_off_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE file_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE period_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE reset_password_request_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE schedule_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE service_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE account (id INT NOT NULL, description TEXT DEFAULT NULL, experience VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE appointment (id INT NOT NULL, patient_id INT NOT NULL, doctor_id INT NOT NULL, service_id INT NOT NULL, date DATE NOT NULL, start_time VARCHAR(5) NOT NULL, end_time VARCHAR(5) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE account (id INT NOT NULL, description TEXT DEFAULT NULL, experience VARCHAR(255) DEFAULT NULL, duration INT NOT NULL, cleanup INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE appointment (id INT NOT NULL, patient_id INT NOT NULL, doctor_id INT NOT NULL, service_id INT NOT NULL, date DATE NOT NULL, start_time TIME(0) WITHOUT TIME ZONE NOT NULL, end_time TIME(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_FE38F8446B899279 ON appointment (patient_id)');
         $this->addSql('CREATE INDEX IDX_FE38F84487F4FB17 ON appointment (doctor_id)');
         $this->addSql('CREATE INDEX IDX_FE38F844ED5CA9E6 ON appointment (service_id)');
@@ -43,17 +42,15 @@ final class Version20230221213034 extends AbstractMigration
         $this->addSql('CREATE TABLE file (id INT NOT NULL, sender_id INT NOT NULL, appointment_id INT NOT NULL, name VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_8C9F3610F624B39D ON file (sender_id)');
         $this->addSql('CREATE INDEX IDX_8C9F3610E5B533F9 ON file (appointment_id)');
-        $this->addSql('CREATE TABLE period (id INT NOT NULL, schedule_id INT NOT NULL, start_time VARCHAR(5) NOT NULL, end_time VARCHAR(5) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_C5B81ECEA40BC2D5 ON period (schedule_id)');
         $this->addSql('CREATE TABLE reset_password_request (id INT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_7CE748AA76ED395 ON reset_password_request (user_id)');
         $this->addSql('COMMENT ON COLUMN reset_password_request.requested_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN reset_password_request.expires_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE schedule (id INT NOT NULL, doctor_id INT NOT NULL, day VARCHAR(9) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE schedule (id INT NOT NULL, doctor_id INT NOT NULL, day VARCHAR(9) NOT NULL, start_time TIME(0) WITHOUT TIME ZONE NOT NULL, end_time TIME(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_5A3811FB87F4FB17 ON schedule (doctor_id)');
-        $this->addSql('CREATE TABLE service (id INT NOT NULL, doctor_id INT NOT NULL, name VARCHAR(255) NOT NULL, duration INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE service (id INT NOT NULL, doctor_id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_E19D9AD287F4FB17 ON service (doctor_id)');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, account_id INT DEFAULT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, is_verified BOOLEAN NOT NULL, profile_picture VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(10) DEFAULT NULL, social_number VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_active BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, account_id INT DEFAULT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, is_verified BOOLEAN NOT NULL, profile_picture VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(10) DEFAULT NULL, social_number VARCHAR(15) DEFAULT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_active BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6499B6B5FBA ON "user" (account_id)');
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
@@ -74,7 +71,6 @@ final class Version20230221213034 extends AbstractMigration
         $this->addSql('ALTER TABLE day_off ADD CONSTRAINT FK_926C726C87F4FB17 FOREIGN KEY (doctor_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610F624B39D FOREIGN KEY (sender_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE file ADD CONSTRAINT FK_8C9F3610E5B533F9 FOREIGN KEY (appointment_id) REFERENCES appointment (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE period ADD CONSTRAINT FK_C5B81ECEA40BC2D5 FOREIGN KEY (schedule_id) REFERENCES schedule (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE schedule ADD CONSTRAINT FK_5A3811FB87F4FB17 FOREIGN KEY (doctor_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE service ADD CONSTRAINT FK_E19D9AD287F4FB17 FOREIGN KEY (doctor_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -90,7 +86,6 @@ final class Version20230221213034 extends AbstractMigration
         $this->addSql('DROP SEQUENCE contact_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE day_off_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE file_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE period_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE reset_password_request_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE schedule_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE service_id_seq CASCADE');
@@ -101,7 +96,6 @@ final class Version20230221213034 extends AbstractMigration
         $this->addSql('ALTER TABLE day_off DROP CONSTRAINT FK_926C726C87F4FB17');
         $this->addSql('ALTER TABLE file DROP CONSTRAINT FK_8C9F3610F624B39D');
         $this->addSql('ALTER TABLE file DROP CONSTRAINT FK_8C9F3610E5B533F9');
-        $this->addSql('ALTER TABLE period DROP CONSTRAINT FK_C5B81ECEA40BC2D5');
         $this->addSql('ALTER TABLE reset_password_request DROP CONSTRAINT FK_7CE748AA76ED395');
         $this->addSql('ALTER TABLE schedule DROP CONSTRAINT FK_5A3811FB87F4FB17');
         $this->addSql('ALTER TABLE service DROP CONSTRAINT FK_E19D9AD287F4FB17');
@@ -111,7 +105,6 @@ final class Version20230221213034 extends AbstractMigration
         $this->addSql('DROP TABLE contact');
         $this->addSql('DROP TABLE day_off');
         $this->addSql('DROP TABLE file');
-        $this->addSql('DROP TABLE period');
         $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE schedule');
         $this->addSql('DROP TABLE service');
